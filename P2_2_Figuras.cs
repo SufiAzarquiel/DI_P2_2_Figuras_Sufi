cad3ddd8d158a6250987251f1beb609aab10b191
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2023-10-09 09:14:20 +0200

    Se me habia olvidado imprimir el area

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -14,21 +14,32 @@ public class P2_2_Figuras
         Circulo circulo = new Circulo(2, Color.FromArgb(255, 50, 17, 200));
         Console.WriteLine("Creando instancia de Circulo...");
         Console.WriteLine(circulo);
+        Console.WriteLine($"Area: {circulo.GetArea()}");
+
         Rectangulo rectangulo = new Rectangulo(7, 10, Color.FromArgb(255, 89, 65, 147));
         Console.WriteLine("Creando instancia de Rectangulo...");
         Console.WriteLine(rectangulo);
+        Console.WriteLine($"Area: {rectangulo.GetArea()}");
+
         Trianguloequilatero triangulo = new Trianguloequilatero(9, Color.FromArgb(255, 255, 10, 10));
         Console.WriteLine("Creando instancia de Triangulo equilatero...");
         Console.WriteLine(triangulo);
+        Console.WriteLine($"Area: {triangulo.GetArea()}");
 
         // Modificamos un valor cualquiera e imprimimos otra vez
         Console.WriteLine("Modificando un valor de cada figura...");
         circulo.Radio = 6;
         rectangulo.Ancho = 20;
         triangulo.Lado = 8;
+
         Console.WriteLine(circulo);
+        Console.WriteLine($"Area: {circulo.GetArea()}");
         Console.WriteLine(rectangulo);
+        Console.WriteLine($"Area: {rectangulo.GetArea()}");
         Console.WriteLine(triangulo);
+        Console.WriteLine($"Area: {triangulo.GetArea()}");
+
+        // Crea
     }
 
 }
