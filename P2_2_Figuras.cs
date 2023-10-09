ae59e8516252bbb81fc1aa9addc49632b105e823
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2023-10-09 09:11:39 +0200

    Parte 3 del ejercicio

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -8,9 +8,27 @@ namespace P2_2_Figuras;
 
 public class P2_2_Figuras
 {
-  static void Main(string[] args)
-  {
+    static void Main(string[] args)
+    {
+        // Crear instancia de cada uno de los tipos de figuras
+        Circulo circulo = new Circulo(2, Color.FromArgb(255, 50, 17, 200));
+        Console.WriteLine("Creando instancia de Circulo...");
+        Console.WriteLine(circulo);
+        Rectangulo rectangulo = new Rectangulo(7, 10, Color.FromArgb(255, 89, 65, 147));
+        Console.WriteLine("Creando instancia de Rectangulo...");
+        Console.WriteLine(rectangulo);
+        Trianguloequilatero triangulo = new Trianguloequilatero(9, Color.FromArgb(255, 255, 10, 10));
+        Console.WriteLine("Creando instancia de Triangulo equilatero...");
+        Console.WriteLine(triangulo);
 
-  }
+        // Modificamos un valor cualquiera e imprimimos otra vez
+        Console.WriteLine("Modificando un valor de cada figura...");
+        circulo.Radio = 6;
+        rectangulo.Ancho = 20;
+        triangulo.Lado = 8;
+        Console.WriteLine(circulo);
+        Console.WriteLine(rectangulo);
+        Console.WriteLine(triangulo);
+    }
 
 }
